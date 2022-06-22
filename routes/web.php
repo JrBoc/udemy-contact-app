544096c1f4c9d562785a1e21775f93694c9d1b90
John Rhey <jrboc18@gmail.com>
2022-06-23 01:49:37 +0800

    24

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contacts', function () {
+    return '<h1>All Contacts</h1>';
+});
+
+Route::get('/contacts/create', function() {
+   return '<h1>Add New Contact</h1>';
+});
+
+Route::get('/contacts/{id}', function($id) {
+    return \App\Models\Contact::find($id);
 });
