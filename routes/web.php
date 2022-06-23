33c5b72b965fdf28f3402e542b45317eca752afb
John Rhey <jrboc18@gmail.com>
2022-06-23 23:00:47 +0800

    47

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,7 @@ Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts
 Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
 Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
