6a6cd395fdf4475fe57161339a7f34889569eb8b
John Rhey <jrboc18@gmail.com>
2022-06-25 02:56:00 +0800

    65

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Settings\AccountController;
 use Illuminate\Support\Facades\Auth;
@@ -12,13 +13,10 @@ Route::redirect('/', '/login');
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-    Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
-    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::resources([
+        '/contacts' => ContactController::class,
+        '/companies' => CompanyController::class,
+    ]);
 
     Route::group(['middleware' => 'password.confirm'], function () {
         Route::get('/settings/account', [AccountController::class, 'index'])->name('settings.account.index');
